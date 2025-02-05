0731d095df50cc9bf332ed55558eb1644be4fb87
dimitri78700 <dimitriraievsky@icloud.com>
2025-02-05 16:57:31 +0100

    Update UserController to handle complete user information submission

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class UserController extends AbstractController
 {
@@ -18,15 +19,21 @@ final class UserController extends AbstractController
     }
 
     #[Route('/complete', name: 'app_complete', methods: ['POST'])]
-    public function complete(Request $request): Response
+    public function complete(Request $request, EntityManagerInterface $em): Response
     {
-        $username = $request->request->get('username');
-        $fullname = $request->request->get('fullname');
+        $username = $request->getPayload()->get('username');
+        $fullname = $request->getPayload()->get('fullname');
 
         if (!empty($username) && !empty($fullname)) {
-            
+            $user = $this->getUser();
+            $user->setUsername($username)
+                 ->setFullname($fullname);
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Votre email a bien été vérifié, Merci.');
+        } else {
+            $this->addFlash('error', 'Veuillez remplir tous les champs.');
         }
-        $this->addFlash('success', 'Votre email a bien été vérifié, Merci.');
         return $this->redirectToRoute('app_profile');
     }
 }
