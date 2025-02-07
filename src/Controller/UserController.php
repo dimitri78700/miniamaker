bf6e704eddb70077ba5a13cf71da2547fbd9d6f5
dimitri78700 <dimitriraievsky@icloud.com>
2025-02-07 14:34:02 +0100

    Update dependencies and add SVG icons

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,37 +2,53 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_profile', methods: ['GET', 'POST'])]
-    public function index(): Response
+    #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+
+            // Redirection avec flash message
+            $this->addFlash('success', 'Votre profil à été mis à jour');
+            return $this->redirectToRoute('app_profile');
+        }
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'userForm' => $form,
         ]);
     }
 
     #[Route('/complete', name: 'app_complete', methods: ['POST'])]
     public function complete(Request $request, EntityManagerInterface $em): Response
     {
-        $username = $request->getPayload()->get('username');
-        $fullname = $request->getPayload()->get('fullname');
-
-        if (!empty($username) && !empty($fullname)) {
-            $user = $this->getUser();
-            $user->setUsername($username)
-                 ->setFullname($fullname);
-            $em->persist($user);
-            $em->flush();
-            $this->addFlash('success', 'Votre email a bien été vérifié, Merci.');
+        $data = $request->getPayload(); // on récupère les données du formulaire
+        if (!empty($data->get('username')) && !empty($data->get('fullname'))) {
+            // Enregistrer les données dans la base de données
+            $user = $this->getUser(); // Ici on récupère l'utilisateur actuel
+            $user
+                ->setUsername($data->get('username')) // on met à jour username
+                ->setFullname($data->get('fullname')) // on met à jour fullname
+                ;
+            $em->persist($user); // on persiste l'utilisateur
+            $em->flush(); // on sauvegarde les modifications en base de données
+            
+            // Redirection avec flash message
+            $this->addFlash('success', 'Votre profil est complété');
         } else {
-            $this->addFlash('error', 'Veuillez remplir tous les champs.');
+            $this->addFlash('error', 'Vous devez remplir tous les champs');
         }
         return $this->redirectToRoute('app_profile');
     }
--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -2,41 +2,118 @@
 
 namespace App\Form;
 
-use App\Entity\Detail;
-use App\Entity\Subscription;
 use App\Entity\User;
-use Symfony\UX\Dropzone\Form\DropzoneType;
-use PhpParser\Node\Stmt\Label;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\UX\Dropzone\Form\DropzoneType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('password', RepeatedType::class, [])
-
-            ->add('username', TextType::class)
-            ->add('fullname', TextType::class)
-
-            ->add('image', DropzoneType::class, [
-                'mapped' => false, // Deconnexion du lien avec l'entité
-                
+            ->add('email', EmailType::class, [
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Votre adresse e-mail',
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une adresse email'
+                    ]),
+                    new Email([
+                        'message' => 'L\'adresse email {{ value }} n\'est pas valide'
+                    ])
+                ]
             ])
-
-            // Prêt a ajouter le profil de l'utilisateur
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Saisisez votre mot de passe pour mettre à jour votre profil',
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => [
+                    'placeholder' => 'Mot de passe',
+                    'class' => 'form-control',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe'
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ])
+                ]
+            ])
+            ->add('username', TextType::class, [
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => "Votre nom d'utilisateur",
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom d\'utilisateur'
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre nom d\'utilisateur doit faire au moins {{ limit }} caractères',
+                        'max' => 50,
+                        'maxMessage' => 'Votre nom d\'utilisateur ne peut pas dépasser {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('fullname', TextType::class, [
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Votre nom complet',
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre nom complet'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre nom complet doit faire au moins {{ limit }} caractères',
+                        'max' => 100,
+                        'maxMessage' => 'Votre nom complet ne peut pas dépasser {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('image', DropzoneType::class, [
+                'mapped' => false,
+                'required' => false,
+                'row_attr' => ['class' => 'my-4 bg-white'],
+                'label' => false,
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => [
+                    'class' => 'form-control my-3',
+                    'placeholder' => 'Mettre à jour votre photo de profil',
+                ],
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '10M',
+                        'maxSizeMessage' => 'Votre image ne doit pas dépasser {{ limit }}',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPG, PNG)',
+                    ])
+                ],
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Modifier votre profil',
-                'attr' => ['class' => 'btn btn-primary']
+                'label' => 'Modifier mon profil',
+                'attr' => ['class' => 'btn btn-primary'],
             ])
         ;
     }
