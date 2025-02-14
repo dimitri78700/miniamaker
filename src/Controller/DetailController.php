8c10be6740c544cc63e11a29f442b719b4da7c82
dimitri78700 <dimitriraievsky@icloud.com>
2025-02-14 10:38:17 +0100

    Refactoriser le code pour gérer les messages dans les discussions

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -2,20 +2,27 @@
 namespace App\Controller;
 use App\Entity\Detail;
 use App\Form\DetailFormType;
+use App\Repository\DetailRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class DetailController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private DetailRepository $dr
+    ){}
     #[Route('/detail', name: 'app_detail')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request): Response
     {
         $detail = new Detail();
         $detail->setPro($this->getUser());
         $form = $this->createForm(DetailFormType::class, $detail);
         $form->handleRequest($request);
+
         if($form->isSubmitted() && $form->isValid()) {
             $role = $form->get('role')->getData();
             if (!empty($role)) {
@@ -26,9 +33,9 @@ final class DetailController extends AbstractController
                 }
             }
             
-            $em->persist($this->getUser());
-            $em->persist($detail);
-            $em->flush();
+            $this->em->persist($this->getUser());
+            $this->em->persist($detail);
+            $this->em->flush();
             
             // Redirection avec flash message
             $this->addFlash('success', 'Félicitations ! Votre fiche est compléte.');
